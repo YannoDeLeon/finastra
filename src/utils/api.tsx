4c25cf33f22b9469e5a50a983abe1533830fa6e9
YannoDeLeon <mjrdeleon19@gmail.com>
2023-03-25 21:57:02 +0800

    env setup for exchange rate

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -4,7 +4,7 @@ export const URLs = {
   COURSES: 'https://run.mocky.io/v3/34bdbb5f-70c0-41ce-aa0c-2bf46befa477',
   PROFILE: 'https://run.mocky.io/v3/214aef9d-b18a-4188-b55f-a25046408a7e',
   CURRENCY: 'https://gist.githubusercontent.com/JCGonzaga01/9f93162c5fb799b7c084bb28fc69a2f1/raw/94c55f89dc4c1e2e7ca49de5658c3441a2b348af/Updated-Common-Currency.json',
-  EXCHANGE_RATE: 'https://v6.exchangerate-api.com/v6', // add /YOUR-API-KEY/pair/EUR/GBP
+  EXCHANGE_RATE: 'https://v6.exchangerate-api.com/v6',
 }
 
 const API = {
@@ -26,7 +26,7 @@ const API = {
     )
   },
   getExchangeRate: (from: string, to: string) => {
-    const url = `${URLs.EXCHANGE_RATE}/679aae77947f03c9abd287ec/pair/${from}/${to}`
+    const url = `${URLs.EXCHANGE_RATE}/${process.env.REACT_APP_EXCHANGERATE_API_KEY}/pair/${from}/${to}`
     const result = API.get(url)
     return result
   }
