96cd26da9d78639850905f00b8f2842b30a73b02
YannoDeLeon <mjrdeleon19@gmail.com>
2023-03-25 08:05:57 +0800

    students/user page fuzzy search initial attempt

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,12 +1,16 @@
-import { useContext, useState } from 'react'
+import { useContext, useState, useRef, SyntheticEvent } from 'react'
 import classes from './students.module.css'
 import AppContext, { TStudentProfile } from '../../store'
-
+import Utils from '../../utils'
 import StudentTableRow from './table-row'
 
 const StudentsPage = () => {
   const { studentProfiles, isLoading, sortData } = useContext(AppContext)
+
+  const searchRef = useRef<HTMLInputElement>(null);
   const [ order, setOrder ] = useState({column: '', type: ''})
+  // move displayed data to State
+  // const [ studentList, setStudentList ] = useState<TStudentProfile>()
 
   const sortHandler = (col: string) => {
     if(order.column === col) {
@@ -19,11 +23,41 @@ const StudentsPage = () => {
     }
   }
 
+  const handleSearch = (e: SyntheticEvent) => {
+    e.preventDefault()
+    const term = searchRef.current?.value.toLowerCase()
+    if(studentProfiles && studentProfiles.length && term) {
+      // const objectsArray = studentProfiles[0]
+
+      // const searchResult = objectsArray.filter((obj) => {
+        const rate = .5
+        const obj = studentProfiles[0]
+        console.log("[obj]", obj)
+        const comparison = Object.keys(obj).map((key) => {
+          console.log("[key]", key)
+          const result = Utils.fuzzySearch(term, obj[key as keyof TStudentProfile].toString())
+          console.log("[result]", result)
+          return result
+        })
+        console.log('[comparison]', comparison)
+
+        // return true for match word > 50%
+
+        return rate
+      // })
+
+
+
+    }
+  }
 
 
   return(
     <div className={classes.container}>
-      {/* <input className={classes.search} type='text' name="search" /> */}
+      <form onSubmit={(e) => handleSearch(e)}>
+        <input className={classes.search} type='text' name="search" ref={searchRef} required />
+        <button>Search</button>
+      </form>
         <table>
           <thead className={classes.header}>
             <tr>
